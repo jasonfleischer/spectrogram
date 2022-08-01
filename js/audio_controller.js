97f08bc9d35a6bdbfd27ccaed1cb5ad56ef0807f
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-08-01 18:39:52 -0400

    update

--- js/audio_controller.js
+++ js/audio_controller.js
@@ -8,42 +8,23 @@ audio_controller = {
 
 audio_controller.startRecording = function() {
 
-let audioIN = { audio: true };
-    //  audio is true, for recording
- 
-    // Access the permission for use
-    // the microphone
+	let audioIN = { audio: true };
     navigator.mediaDevices.getUserMedia(audioIN)
  
-      // 'then()' method returns a Promise
       .then(function (mediaStreamObj) {
  
-        // Connect the media stream to the
-        // first audio element
-        //let audio = document.querySelector('audio');
-
         let audio = document.getElementById('audio');
 
         let audio1 = document.getElementById('audio1');
-
-        //returns the recorded audio via 'audio' tag
- 
-        // 'srcObject' is a property which
-        // takes the media object
-        // This is supported in the newer browsers
         
         audio.srcObject = mediaStreamObj;
 
 
         audio_controller.ctx = new AudioContext();
         audio_controller.analyzerNode = audio_controller.ctx.createAnalyser();
-        
-    	audio_controller.analyzerNode.fftSize = 2048;
+    	audio_controller.analyzerNode.fftSize = 2048;// 256;//32;//2048;
+    	audio_controller.analyzerNode.smoothingTimeConstant = 0;
 
-
-
-       // 
-       
        log.e(mediaStreamObj);
        log.e(audio1);
        log.e(audio1.srcObject)
@@ -54,15 +35,96 @@ let audioIN = { audio: true };
         
 		const captureStream = audio1.captureStream();
         let audioTracks = captureStream.getAudioTracks();
-  		let source = audioCtx.createMediaStreamTrackSource(audioTracks[0]);
+  		let source = audio_controller.ctx.createMediaStreamTrackSource(audioTracks[0]);
         
         source.connect(audio_controller.analyzerNode);
         oscilloscope.draw(audio_controller.analyzerNode);
+
+        const bufferLength = audio_controller.analyzerNode.frequencyBinCount; //half the value of fftSize
+		const dataArray = new Float32Array(bufferLength);
+        draw(audio_controller.analyzerNode, dataArray, bufferLength)
     });
 }
 
+const canvas = document.getElementById('frequency_canvas');
+const canvasCtx = canvas.getContext('2d');
+canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+function draw(analyzerNode, dataArray, bufferLength) {
+  //Schedule next redraw
+  //requestAnimationFrame(draw);
+
+  //var analyzerNode = audio_controller.analyzerNode;
+  //log.e('gg'+analyzerNode)
+  window.requestAnimationFrame(function() {
+	    draw(analyzerNode, dataArray, bufferLength);
+  });
+
+  //Get spectrum data
+  analyzerNode.getFloatFrequencyData(dataArray);
+
+  //Draw black background
+  canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  //Draw spectrum
+  const barWidth = (canvas.width / bufferLength) * 2.5;
+  let posX = 0;
+  for (let i = 0; i < bufferLength; i++) {
+    const barHeight = (dataArray[i] + 140) * 2;
+    canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
+    canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+    posX += barWidth + 1;
+  }
+};
+
+
+
+
+//import chroma from "chroma-js";
+var spectro = new Spectrogram(document.getElementById('spectoCanvas'), {
+  audio: {
+    enable: false
+  }/*,
+
+  colors: function(steps) {
+    var baseColors = [[0,0,255,1], [0,255,255,1], [0,255,0,1], [255,255,0,1], [ 255,0,0,1]];
+    var positions = [0, 0.15, 0.30, 0.50, 0.75];
+ 
+    var scale = new chroma.scale(baseColors, positions)
+    .domain([0, steps]);
+ 
+    var colors = [];
+ 
+    for (var i = 0; i < steps; ++i) {
+      var color = scale(i);
+      colors.push(color.hex());
+    }
+ 
+    return colors;
+  }*/
+});
 audio_controller.stopRecording = function() {
 
+	navigator.getUserMedia({
+	  video: false,
+	  audio: true
+	},
+	function(stream) {
+
+		var audioContext = new AudioContext();
+	  var input = audioContext.createMediaStreamSource(stream);
+	  var analyser = audioContext.createAnalyser();
+	 
+	  analyser.smoothingTimeConstant = 0;
+	  analyser.fftSize = 2048;
+	 
+	  input.connect(analyser);
+	 
+	  spectro.connectSource(analyser, audioContext);
+	  spectro.start();
+	}, function(error) {
+	 
+	});
 
 }
 
